a6259f29f83e44eb873b501b7a53c67869e928d1
Peter <ripiter15@wp.pl>
2019-08-12 09:33:42 +0200

    Add project files.

---
+++ BasicConceptsInCSharp/Program.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace BasicConceptsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lesson0 lesson0 = new Lesson0();
+            bool bit;
+            char character;
+            string value;
+
+            byte small;
+            int bigger;
+            float typicalWithComma;
+            double typicalWithHugeNumbers;
+            long longComma;
+            decimal bigestNumber;
+
+
+            
+
+            
+            Console.ReadKey(true);
+            
+        }
+    }
+}
