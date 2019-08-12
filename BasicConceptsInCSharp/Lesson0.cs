3b99841c8b4a08c1195bfb13a4de66f8088c7f1b
Peter <ripiter15@wp.pl>
2019-08-12 10:08:31 +0200

    Lesson 0, Clean

--- BasicConceptsInCSharp/Lesson0.cs
+++ BasicConceptsInCSharp/Lesson0.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BasicConceptsInCSharp
 {
     class Lesson0
     {
+        void Method()
+        {
+            bool trueOrFalse;
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+
+            float[] flotArray = new float[] { 4f, 3f };
+
+            // flotArray.Length is the amount of values in a array
+            for (int i = 0; i < flotArray.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+            string strigs;
+
+            byte bytes;
+            int inter;
+            float floats = 1.56545616f;
+            double doubles = 214124124;
+            decimal decimals;
+
+            Console.WriteLine("hi");
+
+
+
+        }
     }
 }
--- BasicConceptsInCSharp/Program.cs
+++ BasicConceptsInCSharp/Program.cs
@@ -5,44 +5,10 @@ namespace BasicConceptsInCSharp
     class Program
     {
         static void Main(string[] args)
-        {
-            Lesson0 lesson0 = new Lesson0();
-
-            bool trueOrFalse;
-
-            Console.WriteLine("Hello World");
-            Console.WriteLine();
-
-            float[] flotArray = new float[] { 4f, 3f };
-
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine(i);
-            }
-
-
-
-
-            string strigs;
-
-            byte bytes;
-            int inter;
-            float floats = 1.56545616f;
-            double doubles = 214124124;
-            decimal decimals;
-
-            Console.WriteLine("hi");
-
-
-
+        {           
 
 
             Console.ReadKey(true);
-
-        }
-
-        void Calculating()
-        {
 
         }
     }
