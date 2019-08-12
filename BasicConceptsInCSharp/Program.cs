196cfad52b5c66454e4b6c40aa47280fa0f051be
Peter <ripiter15@wp.pl>
2019-08-12 10:06:35 +0200

    Lesson0 ended

--- BasicConceptsInCSharp/Program.cs
+++ BasicConceptsInCSharp/Program.cs
@@ -7,23 +7,43 @@ namespace BasicConceptsInCSharp
         static void Main(string[] args)
         {
             Lesson0 lesson0 = new Lesson0();
-            bool bit;
-            char character;
-            string value;
 
-            byte small;
-            int bigger;
-            float typicalWithComma;
-            double typicalWithHugeNumbers;
-            long longComma;
-            decimal bigestNumber;
+            bool trueOrFalse;
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+
+            float[] flotArray = new float[] { 4f, 3f };
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
 
 
-            
 
-            
+
+            string strigs;
+
+            byte bytes;
+            int inter;
+            float floats = 1.56545616f;
+            double doubles = 214124124;
+            decimal decimals;
+
+            Console.WriteLine("hi");
+
+
+
+
+
             Console.ReadKey(true);
-            
+
+        }
+
+        void Calculating()
+        {
+
         }
     }
 }
